5d9284d0a18773354ca85c8dd1172d64a73b2bdf
Rumman Khan <rumman.khan@tothenew.com>
2016-03-08 10:28:21 +0530

    App installed and landing page created

---
+++ app/scripts/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name calculatorApp
+ * @description
+ * # calculatorApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('calculatorApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ui.router',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($stateProvider,$urlRouterProvider,$locationProvider) {
+
+    $urlRouterProvider.otherwise("/landing");
+    $stateProvider
+      .state('landing', {
+        url:'/landing',
+        templateUrl: 'views/landing.html',
+        controller: 'LandingCtrl',
+        controllerAs: 'landing'
+      })
+      .state('main', {
+        url:'/main',
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .state('CalcDetail', {
+        url:'/CalcDetail',
+        templateUrl: 'views/calculator-detail.html',
+        controller: 'CalcCtrl',
+        controllerAs: 'calc'
+      })
+  });
---
+++ app/scripts/controllers/landing.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Rumman on 3/8/2016.
+ */
+
+angular.module('calculatorApp').controller('LandingCtrl',function(){
+  var that=this;
+
+})
